f8285f6f86dc97a321256c68384247561e5f208b
Dennis Schmidt <dg085@hdm-stuttgart.de>
2025-01-13 16:40:05 +0100

    Implemented simple example script

--- object-recognition-service/main.py
+++ object-recognition-service/main.py
@@ -1,5 +1,26 @@
-def main():
-    print("I have been called")
+from ultralytics import YOLOWorld
 
 if __name__ == "__main__":
-    main()
+    print("Starting Object Recognition Service")
+    model = YOLOWorld("beer_detection_model.pt")
+
+    images = [
+        "example-images/image.jpeg",
+        "example-images/image2.jpeg",
+        "example-images/image3.jpeg",
+        "example-images/image4.jpeg",
+        "example-images/image5.jpeg",
+        "example-images/image6.jpeg",
+        "example-images/image7.jpeg",
+        "example-images/image8.jpeg",
+    ]
+
+    for image in images:
+        print("Processing image: ", image)
+        results = model.predict(
+            image,
+            iou=0.3,
+            conf=0.3,
+            agnostic_nms=True,
+            save=False,
+        )
