dec0cabdfed8b433910875c7ae713de476d1cd58
Dennis Schmidt <dg085@hdm-stuttgart.de>
2025-01-13 18:49:10 +0100

    changed back to base model + manual labels instead of "pretrained" model

--- object-recognition-service/main.py
+++ object-recognition-service/main.py
@@ -2,25 +2,31 @@ from ultralytics import YOLOWorld
 
 if __name__ == "__main__":
     print("Starting Object Recognition Service")
-    model = YOLOWorld("beer_detection_model.pt")
+    model = YOLOWorld()
 
-    images = [
-        "example-images/image.jpeg",
-        "example-images/image2.jpeg",
-        "example-images/image3.jpeg",
-        "example-images/image4.jpeg",
-        "example-images/image5.jpeg",
-        "example-images/image6.jpeg",
-        "example-images/image7.jpeg",
-        "example-images/image8.jpeg",
-    ]
+    classes = ["beer bottle", "beer can", "beer glass", "beer mug", "beer stein"]
+    modifiers = ["partial visible", "blurry", "empty"]
 
-    for image in images:
-        print("Processing image: ", image)
-        results = model.predict(
-            image,
-            iou=0.3,
-            conf=0.3,
-            agnostic_nms=True,
-            save=False,
-        )
+    model.set_classes(
+        classes
+        + [" ".join([modifier, cls]) for cls in classes for modifier in modifiers]
+    )
+
+    results = model.predict(
+        [
+            "example-images/image.jpeg",
+            "example-images/image2.jpeg",
+            "example-images/image3.jpeg",
+            "example-images/image4.jpeg",
+            "example-images/image5.jpeg",
+            "example-images/image6.jpeg",
+            "example-images/image7.jpeg",
+        ],
+        iou=0.4,
+        conf=0.5,
+        agnostic_nms=True,
+        save=True,
+        project="./example-images",
+        name="results",
+        exist_ok=True,
+    )
