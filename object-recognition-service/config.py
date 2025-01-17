a2d8f891c291c34d85889d4df46664457a3897ad
Urs Weniger <urs@fryd.app>
2025-01-17 14:47:42 +0100

    feat(recognition): refactor config to dataclass

--- object-recognition-service/config.py
+++ object-recognition-service/config.py
@@ -1,7 +1,11 @@
-CONFIG = {
-    'SCALING_FACTOR': 4,
-    'IOU_THRESHOLD': 0.4,
-    'CONFIDENCE_THRESHOLD': 0.5,
-    'WATER_BOTTLE_PATH': 'example-images/water_bottle.png',
-    'OUTPUT_DIR': './example-images/results'
-}
+from dataclasses import dataclass
+
+@dataclass
+class Config:
+    SCALING_FACTOR: float = 4.0
+    IOU_THRESHOLD: float = 0.4
+    CONFIDENCE_THRESHOLD: float = 0.5
+    WATER_BOTTLE_PATH: str = 'example-images/water_bottle.png'
+    OUTPUT_DIR: str = 'example-images/results'
+
+CONFIG = Config()
